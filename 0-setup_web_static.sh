fc24881d6692df66db61ed447d4180a2d1b2c215
Atala2000 <atalakidi@gmail.com>
2023-12-10 15:23:13 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,8 +19,9 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 #editing the nginx config file
-str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}"
-sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
+str=$'\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html 0-index.html 0-index.htm;\n\t}'
+sudo sed -i "/^\tserver_name .*;/a $str" /etc/nginx/sites-enabled/default
+
 
 # restart nginx
 sudo service nginx restart
