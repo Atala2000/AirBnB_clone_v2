6c476fb14035e62f2193cb1f0971d26524aa3db7
Atala2000 <atalakidi@gmail.com>
2023-12-31 16:56:18 +0300

    0: 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
 from flask import Flask
 
-"""
-Module for creating Flask instance
-"""
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def index():
-    """
-    Creates route for /
-    """
+def hello():
+    """display “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    """must be listening on 0.0.0.0, port 5000"""
     app.run(host="0.0.0.0", port=5000)
