fb3bdee328a4edf20a6c1d2fd9a45ae1b739b711
Atala2000 <atalakidi@gmail.com>
2024-01-06 23:41:34 +0300

    change: models/state.py

--- models/state.py
+++ models/state.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class"""
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     @property
     def cities(self):
         """getter cities attributes from file storage"""
         from models import storage
+
         city_list = []
-        for city in storage.all(city).values():
+        for city in storage.all(City).values():
             if city.state_id == self.id:
                 city_list.append(city)
         return city_list
